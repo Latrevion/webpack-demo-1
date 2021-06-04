aaec3434f5beb4bb81ca993489fa0a970b91b044
Latrevion <xiaopikaqiu@outlook.com>
2021-06-04 10:33:32 +0800

    update

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -2,21 +2,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
-const base = require("./webpack.config.base.js");
-
 module.exports = {
-  ...base,
-  mode: "development",
   devtool: "inline-source-map",
   devServer: {
     contentBase: "./dist",
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Latrevion and luke",
+      template: "src/assets/index.html",
+    }),
+  ],
 };
